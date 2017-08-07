a60a836c72037922f898cd03d2949a7ca7c3c6c1
Michael de Wal <michaeldewal@gmail.com>
2017-08-08 01:59:47 +0900

    Fix small bug login

--- app/server/accounts.js
+++ app/server/accounts.js
@@ -2,16 +2,18 @@ Accounts.onCreateUser(function (options, user) {
 
   const groups = options.profile.groups;
 
-  groups.forEach(group => {
-    Invites.insert({
-      "user" : user._id,
-      "reason" : "Already invited",
-      "group" : group,
-      "dateRequested" : new Date(),
-      "processed" : true,
-      "approved" : true
+  if(groups && groups.length > 0) {
+    groups.forEach(group => {
+      Invites.insert({
+        "user" : user._id,
+        "reason" : "Already invited",
+        "group" : group,
+        "dateRequested" : new Date(),
+        "processed" : true,
+        "approved" : true
+      });
     });
-  });
+  }
 
   if (options.profile)
     user.profile = options.profile;
