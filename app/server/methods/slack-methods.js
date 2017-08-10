7e6cccb3d51fe3e957b185cddd7a67c1fe6a440e
Michael de Wal <michaeldewal@gmail.com>
2017-08-10 11:47:54 +0900

    Change slack api method

--- app/server/methods/slack-methods.js
+++ app/server/methods/slack-methods.js
@@ -3,7 +3,7 @@ import slackApiKey from '/imports/helpers/slack-api-key';
 Meteor.methods({
   getGroupsList() {
     const groupId = Meteor.user().profile.identity.team.id;
-    const apiKey = slackApiKey(groupId);
+    const apiKey = (Meteor.user().services.slack.accessToken) ? Meteor.user().services.slack.accessToken : slackApiKey(groupId);
     console.log(apiKey);
     const groups = SlackAPI.groups.list(apiKey);
     console.log(groups);
